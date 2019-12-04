a1fd8a1592b8f5c5386cf8a3fa74b075f0ff2efc
Theo Larefalk <theo.larefalk@learnet.se>
2019-12-04 12:53:40 +0100

    Mind the loop

--- Mind-The.Number.py
+++ Mind-The.Number.py
@@ -1,12 +1,22 @@
 import random
-number = random.randint (1,10)
-print (number)
 
-
-
-
-
-
+loop = True
+loop2 = True
+while loop2==True:
+    number = random.randint (1,10)
+    while loop==True:
+        print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+        val = int(input("Gissa nummer: "))
+        if val == number:
+            print ("Du gissa rätt")
+            loop = False
+        elif val > number:
+            print ("Du gissa för högt")
+        elif val < number:
+            print ("Du gissa för lågt")
+        else:
+            loop=False
+            exit()
 
 
 
