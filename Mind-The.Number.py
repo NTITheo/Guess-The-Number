9898d6a38928efa09212aa980bdaf452caee3f80
Theo Larefalk <theo.larefalk@learnet.se>
2019-12-04 13:14:12 +0100

    Mind the Number 1.5 the loopening

--- Mind-The.Number.py
+++ Mind-The.Number.py
@@ -4,8 +4,9 @@ loop = True
 loop2 = True
 while loop2==True:
     number = random.randint (1,10)
+    print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+    loop=True
     while loop==True:
-        print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
         val = int(input("Gissa nummer: "))
         if val == number:
             print ("Du gissa rätt")
